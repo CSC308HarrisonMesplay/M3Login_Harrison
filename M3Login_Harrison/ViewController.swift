b47b08f44b14c7302262b2a5518500ef59be6b78
Harrison <h.mesplay@gmail.com>
2026-02-03 12:08:26 -0500

    commit

--- M3Login_Harrison/ViewController.swift
+++ M3Login_Harrison/ViewController.swift
@@ -9,6 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var pwTextField: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        let id = idTextField.text!
+        let pw = pwTextField.text!
+        //id:harrison, pw: 1234
+        
+        if id.isEmpty || pw.isEmpty{
+            resultLabel.text = "Please input ID and Password."
+            return
+        }
+        
+        /*
+        if id == "harrison" && pw == "1234"{
+            resultLabel.text = "Login Success!"
+        }else{
+            resultLabel.text = "Login Failed."
+        }
+         */
+        
+        resultLabel.text = (id == "harrison" && pw == "1234") ?
+        "Login Success!" : "Login Failed."
+    }
+    
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- Swift/MyPlayground.playground/Contents.swift
+++ Swift/MyPlayground.playground/Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Logical Operator: AND(&&), OR(||), NOT(!)
+ */
+
+true && true
+true && false
+false && true
+false && false
+
+true || true
+true || false
+false || true
+false || false
+
+!true
+!false
+
+//ternary
+//condition ? valueIfTrue: valueIfFalse
+
+let age: Int = 18
+if age >= 18 {
+    print("You are an adult.")
+}else{
+    print( "You are a minor.")
+}
+
+let result: String = age >= 18 ? "You are an adult." : "You are a minor."
+print (result)
+
+//these do the same thing
